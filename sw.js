82d25f35eb76f575dd4e3baf9c04dd8eb950674e
youxunzhang <youxun2023@gmail.com>
2025-08-27 12:39:26 +0800

    66

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'ni-haixia-clinic-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700;900&display=swap'
+];
+
+// 安装service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // 如果找到缓存的响应，则返回缓存
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// 更新缓存
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
